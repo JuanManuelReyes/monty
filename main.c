22264a60ff1b887763eb0ecb78a40c22042b2c34
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-12 15:17:16 -0700

    a

--- main.c
+++ main.c
@@ -37,7 +37,6 @@ int main(int argc, char *argv[])
 		token = strtok(line, "\n\t $");
 		struc->argument = strtok(NULL, "\n\t $");
 		struc->line = line;
-		/*if (token == NULL || token[0] == '#')continue;*/
 		if (token != NULL)
 			stack = get_op_func(&stack, token, line_number);
 		line_number++;
